eb73d1d472cb7be9f486bf866acecec34f9ac67c
Cameron Sowder <cameron@sowder.io>
2026-05-26 11:49:38 -0400

    Updated system-update cmd
      - Added some color and made it work on both unix systems i have.

--- local/bin/system-update.sh
+++ local/bin/system-update.sh
@@ -1,11 +1,69 @@
-#!/bin/bin/env bash
+#!/bin/bin/env zsh
 
 function system-update() {
-    softwareupdate --install -all
-    brew cleanup
-    brew update
-    brew upgrade
-    brew upgrade --cask
-    mas upgrade
-    rustup update
+
+  local red=$(tput setaf 1)
+  local reset=$(tput sgr0)
+  local green=$(tput setaf 2)
+
+  local help_flag
+  local what_if_flag
+  local "${os_type}"="$(builtin command uname)" || return 1
+
+  local usage="
+    system-update [--help]
+    system-update [--what-if]
+  "
+
+  while [[ "$1" == -* ]]; do
+    case "$1" in
+      --help)
+        help_flag=1
+        shift
+        ;;
+      --what-if)
+        what_if_flag=1
+        shift
+        ;;
+      *)
+        echo "${red}Unknown option: $1${reset}"
+        echo "${red}$usage${reset}"
+        return 1
+        ;;
+    esac
+  done
+
+  [[ -z "${help_flag}" ]] || { echo "${green}$usage${reset}" && return }
+
+  if ! [[ -z $os_type ]]; then
+    if [[ $os_type = *"Darwin"* ]]; then
+      if (( $#what_if_flag )); then
+        softwareupdate --list
+      else
+        softwareupdate --install --all
+      fi
+    else
+      if (($+commands[cachy-update])); then
+        if (( $#what_if_flag )); then
+          checkupdates
+        else
+          cachy-update
+        fi
+      fi
+    fi
+  fi
+
+  if (($+commands[brew])); then
+    if (( $#what_if_flag )); then
+      brew upgrade --dry-run
+      brew upgrade --dry-run --cask
+    else
+      brew cleanup
+      brew update
+      brew upgrade
+      brew upgrade --cask
+    fi
+  fi
+
 }
+
